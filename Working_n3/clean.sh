e05223f6982a26e0a7d1b01c26b247470bc1aad1
albe9 <albertom1999@gmail.com>
2023-02-21 15:35:13 +0100

    Modificato script per estrarre Road

--- Working_n3/clean.sh
+++ Working_n3/clean.sh
@@ -1,12 +1,17 @@
 #!/bin/bash
 
-# var="<http://www.disit.org/km4city/schema#endsAtNode>"
-# echo "${var//<*schema/}"
+grep '201906809' ./../Dockers/maps/tmp/*.n3 > extract.txt
 
+echo > cleaned.txt
 
 while read line;
     do
-        cleaned=${line##<http://www.disit.org/km4city/resource/OS00098463511RE/}
-        cleaned=${cleaned//<*schema/}
+        cleaned=${line//'<http://www.disit.org/km4city/resource/OS00098463511RE/'/}
+        cleaned=${cleaned//'<*schema'/}
         echo $cleaned >> cleaned.txt
+        if [[ "$cleaned" == *"#RoadElement>"* ]];
+        then
+            echo -e "\n" >> cleaned.txt
+        fi
+        
     done < extract.txt
