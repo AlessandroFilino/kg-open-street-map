3f24c0deaf6de42bd4dd8060c2e8dfad958c4881
albe9 <albertom1999@gmail.com>
2023-04-17 17:25:51 +0200

    Rinominata versione

--- Working_n3/clean.sh
+++ Working_n3/clean.sh
@@ -2,7 +2,7 @@
 
 road_id='201906809'
 
-grep $road_id ./../Dockers/maps/tmp/42621_V2.n3 > $road_id.txt
+grep $road_id ./../Dockers/maps/montemignaio/42621.n3 > $road_id.txt
 
 
 echo > "$road_id"_cleaned.txt
