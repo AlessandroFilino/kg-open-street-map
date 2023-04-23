5cca1f96038a42b1bfc6568097653bcceca10dcd
albe9 <albertom1999@gmail.com>
2023-04-23 16:33:18 +0200

    Aggiunto controllo per il load su virtuoso

--- One_shot/one_shot_script.sh
+++ One_shot/one_shot_script.sh
@@ -6,9 +6,9 @@ LOAD_TO_VIRTUOSO=$(grep -Po '(?<=\[Load_To_Virtuoso\] : ")[^"]*' ./setup.config)
 
 cd ./../Dockers
 docker compose up -d
-docker exec -it kg-open-street-map-ubuntu-1 sh -c "/home/scripts/one_shot.sh $RELATION_NAME"
+# docker exec -it kg-open-street-map-ubuntu-1 sh -c "/home/scripts/one_shot.sh $RELATION_NAME"
 
-if [ $? -eq 0 ];
+if [ $? -eq 0 ] && [ $LOAD_TO_VIRTUOSO = "true" ];
 then 
     mkdir ./../Dockers/virtuoso_data/$RELATION_NAME
     cp ./../Dockers/maps/$RELATION_NAME/*.n3 ./../Dockers/virtuoso_data/$RELATION_NAME
