7beb7e9fd92bedbf3970289c4b70e67715e7d7ee
albe9 <albertom1999@gmail.com>
2023-05-13 19:13:57 +0200

    Risolto errore

--- Dockers/scripts/load_to_virtuoso.sh
+++ Dockers/scripts/load_to_virtuoso.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 
 RELATION_NAME=$1
-# GRAPH_NAME=$2
-GRAPH_NAME="http://example.org/test"
+GRAPH_NAME=$2
 
 # Controlliamo se il grafo contiene già delle triple
 GRAPH_EXISTS=$(echo "SPARQL ASK { GRAPH <$GRAPH_NAME> { ?s ?p ?o } };" | isql-vt -H kg-open-street-map-virtuoso-1 -P admin) 
@@ -14,10 +13,12 @@ if [ "$GRAPH_EXISTS" -eq 1 ];
         # Il grafo contiene già delle triple,le eliminiamo 
         echo "\nELIMINAZIONE TRIPLE DA VECCHIO GRAFO $GRAPH_NAME.n3\n"
         echo "SPARQL CLEAR GRAPH <$GRAPH_NAME>;" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
-        echo "DELETE FROM DB.DBA.LOAD_LIST WHERE ll_file='$RELATION_NAME/$RELATION_NAME.n3';" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
     fi
 
-echo "\n\nCARICAMENTO TRIPLE DA FILE : $RELATION_NAME.n3\n"
+# Rimuoviamo il file dalla load list per ricaricarlo
+echo "DELETE FROM DB.DBA.LOAD_LIST WHERE ll_file='$RELATION_NAME/$RELATION_NAME.n3';" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
+
+echo "\n\nCARICAMENTO TRIPLE DA FILE : $RELATION_NAME.n3 NEL GRAFO : $GRAPH_NAME\n"
 # echo "SPARQL CREATE GRAPH <$GRAPH_NAME>;" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
 echo "ld_dir('$RELATION_NAME', '$RELATION_NAME.n3', '$GRAPH_NAME');" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
 echo "rdf_loader_run();" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
