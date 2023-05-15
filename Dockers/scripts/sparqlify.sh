e8d8961e87ce4512d94ed28a68bc142ba26c98d7
AlessandroFilino <65735750+AlessandroFilino@users.noreply.github.com>
2023-05-15 18:42:24 +0200

    Aggiunto supporto ultima versione sparqlify

--- Dockers/scripts/sparqlify.sh
+++ Dockers/scripts/sparqlify.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-java -cp /home/scripts/tools/sparqlify/sparqlify-cli/target/sparqlify-cli-0.8.2-jar-with-dependencies.jar RunEndpoint $@
+java -cp /home/scripts/tools/sparqlify/sparqlify-cli/target/sparqlify-cli-0.9.1-jar-with-dependencies.jar RunEndpoint $@
