f711eba19daa798e6911c3688ee2db705ec3215e
albe9 <albertom1999@gmail.com>
2023-05-17 23:04:04 +0200

    rimossi commenti di test

--- Dockers/scripts/irdbcmap.sh
+++ Dockers/scripts/irdbcmap.sh
@@ -14,8 +14,8 @@ if [ $GENERATE_OLD = "True" ]; then
 fi
 
 psql postgresql://admin:admin@postgres:5432/maps  -f /home/scripts/irdbcmap.sql -v OSM_ID=$OSM_ID
-#/home/scripts/sparqlify.sh -m /home/scripts/irdbcmap.sml -h postgres -d maps -U admin -W admin -o ntriples --dump > /home/maps/$OSM_ID/$OSM_ID.drt
-#cd /home/maps/$OSM_ID/
-#tail -n +3 $OSM_ID.drt > $OSM_ID.cln 
-#sort $OSM_ID.cln | uniq > $OSM_ID.n3 
-#rm $OSM_ID.drt $OSM_ID.cln
+/home/scripts/sparqlify.sh -m /home/scripts/irdbcmap.sml -h postgres -d maps -U admin -W admin -o ntriples --dump > /home/maps/$OSM_ID/$OSM_ID.drt
+cd /home/maps/$OSM_ID/
+tail -n +3 $OSM_ID.drt > $OSM_ID.cln 
+sort $OSM_ID.cln | uniq > $OSM_ID.n3 
+rm $OSM_ID.drt $OSM_ID.cln
