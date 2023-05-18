220ba43b6aacf2088984d62cbc52d734cf52d60a
albe9 <albertom1999@gmail.com>
2023-05-18 20:24:28 +0200

    Aggiunta l'opzione per specificare OSM_ID risolti problemi nella copia delle triple

--- One_shot/one_shot_script.py
+++ One_shot/one_shot_script.py
@@ -19,10 +19,15 @@ def parse_arguments():
                         type=str)
     
     parser.add_argument("-r", "--relation_name",
-                        help="Inserire il nome della relazione su cui si vuole compiere la triplificazione. \n\
-    se fileName non è specificato la mappa verrà scaricata automaticamente tramite geofabrik",
+                        help="Inserire il nome della relazione su cui si vuole compiere la triplificazione, per sceglier un osm_id specifico usare invevce --OSM_ID. \n\
+    Se FILE_NAME non è specificato la mappa verrà scaricata automaticamente tramite geofabrik.",
                         nargs=1,
-                        required=True,
+                        type=str)
+    
+    parser.add_argument("-o", "--osm_id",
+                        help="Inserire l'OSM_ID della relazione su cui si vuole compiere la triplificazione. \n\
+    Se FILE_NAME non è specificato la mappa verrà scaricata automaticamente tramite overpass.",
+                        nargs=1,
                         type=str)
     
     parser.add_argument("-l", "--load_to_rdf",
@@ -35,9 +40,18 @@ def parse_arguments():
                         action="store_true",
                         help="Genera anche un altro file .n3 contenente le triple senza filtri (con tutti i road element intermedi)")
 
-    return vars(parser.parse_args())
+    args = parser.parse_args()
+
+
+    if not (args.relation_name or args.osm_id):
+        parser.error("Nessuna azione da compiere, specificare RELATION_NAME o OSM_ID")
+    elif args.relation_name and args.osm_id:
+        parser.error("RELATION_NAME e OSM_ID specificati, scegliere solo una delle due opzioni")
+
+
+    return vars(args)
     
-def get_relation_data(relation_name):
+def get_relation_data_by_name(relation_name):
     json = requests.get(f"https://nominatim.openstreetmap.org/search.php?q={relation_name}&format=json").json()
     #filtro per estrarre la relazione
     for osm_element in json:
@@ -45,18 +59,28 @@ def get_relation_data(relation_name):
             relation_data = osm_element
             return relation_data
 
-def download_map(relation_name):
-    
-    relation_data = get_relation_data(relation_name)
+def get_relation_data_by_osmid(osm_id):
+    response = requests.get(f"https://nominatim.openstreetmap.org/lookup?osm_ids=R{osm_id}&format=json").json()
+    relation_data = response[0]
+    if relation_data["osm_type"] != "relation":
+        print(f"Errore l'osm_id : {osm_id} non corrisponde ad una relazione")
+        exit(-1)
+    elif relation_data["class"] != "boundary":
+        print(f"Errore l'osm_id : {osm_id} non corrisponde ad un boundary")
+        exit(-1)
 
-    osm_id = relation_data["osm_id"]
-    boundingbox = relation_data["boundingbox"]
+    return relation_data
+    
+
+def download_map(osm_id, bbox):
+   
+    boundingbox = bbox
     for coord_idx in range(4):
         boundingbox[coord_idx] = float(boundingbox[coord_idx])
     
     #verifico che non esistano già dati relativi a questa relazione
     if os.path.exists(f"{BASE_DIR}/Dockers/maps/{osm_id}.osm"):
-        print(f"{relation_name}.osm già presente, la mappa non verrà riscaricata")
+        print(f"{osm_id}.osm già presente, la mappa non verrà riscaricata")
         return osm_id
 
 
@@ -66,7 +90,7 @@ def download_map(relation_name):
            f"relation({boundingbox[0]},{boundingbox[2]},{boundingbox[1]},{boundingbox[3]});"
            f"<;);out%20center%20meta;")
     
-    print(f"Sto scaricando i dati relativi a {relation_name} da open-street-map, attendere")
+    print(f"Sto scaricando i dati relativi a {osm_id} da open-street-map, attendere")
     
     with open(f"{BASE_DIR}/Dockers/maps/{osm_id}.osm", "wb+") as f:
         map_data = requests.get(f"http://overpass-api.de/api/interpreter?data={query}", stream=True)
@@ -74,7 +98,6 @@ def download_map(relation_name):
             f.write(data)
 
     print("Mappa scaricata con successo")
-    return osm_id
 
 def execute_shell_command(command, output=None):
     if (output == None):
@@ -95,38 +118,45 @@ BASE_DIR = pathlib.Path(__file__).parent.parent.resolve()
 
 def main():
     args = parse_arguments()
-    relation_name = args["relation_name"][0]
+    relation_name = None
     file_name = args["file_name"]
     generate_old = args["generate_old"]
     graph_name = None
-    osm_id = None
+    osm_id = args["osm_id"]
     map_type = None
     bbox = [0, 0, 0, 0]
     
     if args["load_to_rdf"] != None:
         graph_name = args["load_to_rdf"][0]
 
+    if args["relation_name"] != None:
+        relation_name = args["relation_name"][0]
+
+
+    if relation_name != None:
+        relation_data = get_relation_data_by_name(relation_name)
+        osm_id = relation_data["osm_id"]
+    elif osm_id != None:
+        relation_data = get_relation_data_by_osmid(osm_id)
+    bbox = relation_data["boundingbox"]
+
+
     if file_name != None:
         file_name = file_name[0]
         map_type = "pbf"
-        relation_data = get_relation_data(relation_name)
-        osm_id = relation_data["osm_id"]
-        bbox = relation_data["boundingbox"]
 
         if not os.path.exists(f"{BASE_DIR}/Dockers/maps/{file_name}"):
             print(f"Impossibile trovare la mappa in {BASE_DIR}/Dockers/maps/{file_name}")
             exit(-1)
 
     else:
-        derived_osm_id = download_map(relation_name)
-        osm_id = derived_osm_id
+        download_map(osm_id, bbox)
         file_name = f"{osm_id}.osm"
         map_type = "osm"
 
     os.chdir(f"{BASE_DIR}/Dockers")
     execute_shell_command(["docker", "compose", "up", "-d"])
 
-    
     timeout = -1
     ready_to_accept_conn = False
     while not ready_to_accept_conn:
@@ -144,7 +174,6 @@ def main():
                 ready_to_accept_conn = True
                 break
         
-
     execute_shell_command(["docker", "exec", "-it", "kg-open-street-map-ubuntu-1", "sh", "-c", "/home/scripts/init.sh"])
     file_name_cleaned = file_name.split(".")[0]
     execute_shell_command(["docker", "exec", "-it", "kg-open-street-map-ubuntu-1", "sh", "-c", f"/home/scripts/load_map.sh {osm_id} {file_name_cleaned} {map_type} {float(bbox[0])} {float(bbox[1])} {float(bbox[2])} {float(bbox[3])}"])
@@ -152,19 +181,19 @@ def main():
 
     if graph_name != None:
         # TODO inserire controllo sul file .n3 per verificare che sia stato generato
-        if os.path.exists(f"{BASE_DIR}/Dockers/virtuoso_data/{osm_id}"):
-            os.rmdir(f"{BASE_DIR}/Dockers/virtuoso_data/{osm_id}")
+        path_in_virtuoso = f"{BASE_DIR}/Dockers/virtuoso_data/{osm_id}" 
+        if os.path.exists(path_in_virtuoso):
+            for triple in os.listdir(path_in_virtuoso):
+                os.remove(path_in_virtuoso + "/" + triple)
+            os.rmdir(path_in_virtuoso)
 
-        os.mkdir(f"{BASE_DIR}/Dockers/virtuoso_data/{osm_id}")
-        shutil.copy2(f"{BASE_DIR}/Dockers/maps/{osm_id}/{osm_id}.n3", f"{BASE_DIR}/Dockers/virtuoso_data/{osm_id}")
+        os.mkdir(path_in_virtuoso)
+        shutil.copy2(f"{BASE_DIR}/Dockers/maps/{osm_id}/{osm_id}.n3", path_in_virtuoso)
         execute_shell_command(["docker", "exec", "-it", "kg-open-street-map-ubuntu-1", "sh", "-c", f"/home/scripts/load_to_virtuoso.sh {osm_id} {graph_name}"])
 
 if __name__ == "__main__":
     main()
-    
-    
 
 
 
 # python3 One_shot/one_shot_script.py -r montemignaio -l http://example.org/test1
-# pg_restore -d maps_old maps_old_db.tar -h postgres -U admin
--- One_shot/test.py
+++ One_shot/test.py
@@ -1,18 +1,20 @@
 #!/usr/bin/env python3
-#execute_shell_command(["docker", "exec", "kg-open-street-map-virtuoso-1", "mkdir", "-p", f"/opt/virtuoso-opensource/database/42621/"])
-#docker cp kg-open-street-map-ubuntu-1:/home/maps/42621/42621.n3 - | docker exec -i kg-open-street-map-virtuoso-1 tar x -C /opt/virtuoso-opensource/database/42621/
 
-import subprocess
+import requests
 
-def execute_shell_command(command):
-    process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    stdout, stderr = process.communicate()
-    return stdout.decode('utf-8').strip(), stderr.decode('utf-8').strip(), process.returncode
+def download_map(relation_id):
+    response = requests.get(f"https://nominatim.openstreetmap.org/lookup?osm_ids=R{relation_id}&format=json").json()
+    print(response)
+    # polygon_coords = response["features"][0]["geometry"]["coordinates"][0]
+    # polygon_coords_str = "poly: "
+    # for (lat,long) in polygon_coords:
+    #     polygon_coords_str += f"{lat} {long} " 
+    # print(polygon_coords_str)
 
-osm_id = 42621
 
-# Esegue il comando completo utilizzando execute_shell_command
-command = [
-    "sh", "-c", f"docker cp kg-open-street-map-ubuntu-1:/home/maps/{osm_id}/{osm_id}.n3 - | docker exec -i kg-open-street-map-virtuoso-1 sh -c 'cd /opt/virtuoso-opensource/database/{osm_id}/ && tar x'"
-]
-stdout, stderr, returncode = execute_shell_command(command)
+download_map(42621)
+
+# (way(poly:");
+# node();
+# rel(););
+# out meta;
