357425ed8ac3049481cce0fa60669cb3fe25a012
AlessandroFilino <65735750+AlessandroFilino@users.noreply.github.com>
2023-05-22 17:42:54 +0200

    bug fix init.sh

--- Dockers/scripts/init.sh
+++ Dockers/scripts/init.sh
@@ -4,6 +4,21 @@ DB_USER="admin"
 DB_NAME="maps"
 DB_PASSWORD="admin"
 
+if [ ! -d /home/scripts/tools/osmosis ]; then
+    mkdir -p /home/scripts/tools/osmosis
+    wget -P /home/scripts/tools/osmosis https://github.com/openstreetmap/osmosis/releases/download/0.48.3/osmosis-0.48.3.tgz 
+    tar xvfz /home/scripts/tools/osmosis/osmosis-0.48.3.tgz -C /home/scripts/tools/osmosis
+    rm /home/scripts/tools/osmosis/osmosis-0.48.3.tgz
+    chmod a+x /home/scripts/tools/osmosis/bin/osmosis      
+fi
+
+if [ ! -d /home/scripts/tools/sparqlify ]; then
+    unzip /home/scripts/sparqlify.zip -d /home/scripts/tools/
+fi
+
+chmod -R 777 /home/scripts/
+chmod -R 777 /home/maps/
+
 psql postgresql://$DB_USER:$DB_PASSWORD@postgres:5432/postgres -c "DROP DATABASE IF EXISTS $DB_NAME;" ||
   { echo "Errore durante l'esecuzione del comando psql: Eliminazione del database"; exit 1; }
 
