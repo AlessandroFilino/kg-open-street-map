1700a6876126a873dd8b9e5c2b7438b4c89ac794
albe9 <albertom1999@gmail.com>
2023-05-23 19:12:56 +0200

    Astratta la psw di virtuoso

--- Dockers/scripts/load_to_virtuoso.sh
+++ Dockers/scripts/load_to_virtuoso.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
+VIRTUOSO_PSW="admin"
+
 RELATION_NAME=$1
 GRAPH_NAME=$2
 
 # Controlliamo se il grafo contiene già delle triple
-GRAPH_EXISTS=$(echo "SPARQL ASK { GRAPH <$GRAPH_NAME> { ?s ?p ?o } };" | isql-vt -H kg-open-street-map-virtuoso-1 -P admin) 
+GRAPH_EXISTS=$(echo "SPARQL ASK { GRAPH <$GRAPH_NAME> { ?s ?p ?o } };" | isql-vt -H kg-open-street-map-virtuoso-1 -P $VIRTUOSO_PSW) 
 GRAPH_EXISTS=$(echo $GRAPH_EXISTS | grep -oP 'ask_retval INTEGER _*?\K ([10])')
 
 
@@ -12,15 +14,15 @@ if [ "$GRAPH_EXISTS" -eq 1 ];
     then
         # Il grafo contiene già delle triple,le eliminiamo 
         echo "\nELIMINAZIONE TRIPLE DA VECCHIO GRAFO $GRAPH_NAME.n3\n"
-        echo "SPARQL CLEAR GRAPH <$GRAPH_NAME>;" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
+        echo "SPARQL CLEAR GRAPH <$GRAPH_NAME>;" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P $VIRTUOSO_PSW
     fi
 
 # Rimuoviamo il file dalla load list per ricaricarlo
-echo "DELETE FROM DB.DBA.LOAD_LIST WHERE ll_file='$RELATION_NAME/$RELATION_NAME.n3';" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
+echo "DELETE FROM DB.DBA.LOAD_LIST WHERE ll_file='$RELATION_NAME/$RELATION_NAME.n3';" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P $VIRTUOSO_PSW
 
 echo "\n\nCARICAMENTO TRIPLE DA FILE : $RELATION_NAME.n3 NEL GRAFO : $GRAPH_NAME\n"
-# echo "SPARQL CREATE GRAPH <$GRAPH_NAME>;" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
-echo "ld_dir('$RELATION_NAME', '$RELATION_NAME.n3', '$GRAPH_NAME');" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
-echo "rdf_loader_run();" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P admin
+
+echo "ld_dir('$RELATION_NAME', '$RELATION_NAME.n3', '$GRAPH_NAME');" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P $VIRTUOSO_PSW
+echo "rdf_loader_run();" | isql-vt isql-vt -H kg-open-street-map-virtuoso-1 -P $VIRTUOSO_PSW
 
 
