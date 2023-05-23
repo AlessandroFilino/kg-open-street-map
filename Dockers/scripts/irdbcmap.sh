ccd8983153ecf0705fe71ced546cc9eab04738e7
albe9 <albertom1999@gmail.com>
2023-05-23 19:13:54 +0200

    Aggiunta gestione di errori e codici di uscita

--- Dockers/scripts/irdbcmap.sh
+++ Dockers/scripts/irdbcmap.sh
@@ -9,16 +9,20 @@ RELATION_NAME=$2
 GENERATE_OLD=$3
 
 if [ $GENERATE_OLD = "True" ]; then
-    psql postgresql://$DB_USER:$DB_PASSWORD@postgres:5432/$DB_NAME  -f /home/scripts/irdbcmap_old.sql -v OSM_ID=$OSM_ID
-    /home/scripts/sparqlify.sh -m /home/scripts/irdbcmap.sml -h postgres -d $DB_NAME -U $DB_USER -W $DB_PASSWORD -o ntriples --dump > /home/maps/$OSM_ID/"$OSM_ID"_old.drt
+    psql postgresql://$DB_USER:$DB_PASSWORD@postgres:5432/$DB_NAME  -f /home/scripts/irdbcmap_old.sql -v OSM_ID=$OSM_ID ||
+  { echo "Errore durante l'esecuzione di /home/scripts/irdbcmap_old.sql sul db"; exit 1; } 
+    /home/scripts/sparqlify.sh -m /home/scripts/irdbcmap.sml -h postgres -d $DB_NAME -U $DB_USER -W $DB_PASSWORD -o ntriples --dump > /home/maps/$OSM_ID/"$OSM_ID"_old.drt ||
+  { echo "Errore durante l'esecuzione di /home/scripts/sparqlify.sh"; exit 1; } 
     cd /home/maps/$OSM_ID/
     tail -n +3 "$OSM_ID"_old.drt > "$OSM_ID"_old.cln 
     sort "$OSM_ID"_old.cln | uniq > "$OSM_ID"_old.n3 
     rm "$OSM_ID"_old.drt "$OSM_ID"_old.cln
 fi
 
-psql postgresql://$DB_USER:$DB_PASSWORD@postgres:5432/$DB_NAME  -f /home/scripts/irdbcmap.sql -v OSM_ID=$OSM_ID
-/home/scripts/sparqlify.sh -m /home/scripts/irdbcmap.sml -h postgres -d $DB_NAME -U $DB_USER -W $DB_PASSWORD -o ntriples --dump > /home/maps/$OSM_ID/$OSM_ID.drt
+psql postgresql://$DB_USER:$DB_PASSWORD@postgres:5432/$DB_NAME  -f /home/scripts/irdbcmap.sql -v OSM_ID=$OSM_ID ||
+  { echo "Errore durante l'esecuzione di /home/scripts/irdbcmap_old.sql sul db"; exit 1; } 
+/home/scripts/sparqlify.sh -m /home/scripts/irdbcmap.sml -h postgres -d $DB_NAME -U $DB_USER -W $DB_PASSWORD -o ntriples --dump > /home/maps/$OSM_ID/$OSM_ID.drt ||
+  { echo "Errore durante l'esecuzione di /home/scripts/sparqlify.sh"; exit 1; } 
 cd /home/maps/$OSM_ID/
 tail -n +3 $OSM_ID.drt > $OSM_ID.cln 
 sort $OSM_ID.cln | uniq > $OSM_ID.n3 
--- Dockers/scripts/load_map.sh
+++ Dockers/scripts/load_map.sh
@@ -20,21 +20,24 @@ else
     mkdir /home/maps/$OSM_ID
 fi
 
-cp /home/scripts/pgsimple_load_0.6.sql /home/maps/$OSM_ID
+cp /home/scripts/pgsimple_load_0.6.sql /home/maps/$OSM_ID ||
+  { echo "Errore durante la copia di /home/scripts/pgsimple_load_0.6.sql in /home/maps/$OSM_ID"; exit 1; }
 
 if [ $MAP_TYPE = "osm" ]; then
-    $osmosis --read-xml  /home/maps/$OSM_ID.osm --log-progress --write-pgsimp-dump directory=/home/maps/$OSM_ID/ enableBboxBuilder=yes enableLinestringBuilder=yes
+    $osmosis --read-xml  /home/maps/$OSM_ID.osm --log-progress --write-pgsimp-dump directory=/home/maps/$OSM_ID/ enableBboxBuilder=yes enableLinestringBuilder=yes ||
+  { echo "Errore durante la lettura di /home/maps/$OSM_ID.osm con osmosis"; exit 1; } 
 elif [ $MAP_TYPE = "pbf" ]; then
     # Filtriamo la mappa in base al bbox di interesse
-    # echo $BBOX_LEFT $BBOX_RIGHT $BBOX_TOP $BBOX_BOTTOM
-    # exit
-    $osmosis --read-pbf /home/maps/$RELATION_NAME.osm.pbf --bounding-box left=$BBOX_LEFT right=$BBOX_RIGHT top=$BBOX_TOP bottom=$BBOX_BOTTOM --log-progress --write-pgsimp-dump directory=/home/maps/$OSM_ID/ enableBboxBuilder=yes enableLinestringBuilder=yes
+    $osmosis --read-pbf /home/maps/$RELATION_NAME.osm.pbf --bounding-box left=$BBOX_LEFT right=$BBOX_RIGHT top=$BBOX_TOP bottom=$BBOX_BOTTOM --log-progress --write-pgsimp-dump directory=/home/maps/$OSM_ID/ enableBboxBuilder=yes enableLinestringBuilder=yes ||
+  { echo "Errore durante la lettura di /home/maps/$OSM_ID.pbf con osmosis"; exit 1; } 
 else
-    echo "Estenzione della mappa invalida: $MAP_TYPE"
+   { echo "Estenzione dell mappa non valida"; exit 1; } 
 fi
 
 # rm /home/maps/$OSM_ID.osm
 cd /home/maps/$OSM_ID/
-psql postgresql://$DB_USER:$DB_PASSWORD@postgres:5432/$DB_NAME -f /home/maps/$OSM_ID/pgsimple_load_0.6.sql
+psql postgresql://$DB_USER:$DB_PASSWORD@postgres:5432/$DB_NAME -f /home/maps/$OSM_ID/pgsimple_load_0.6.sql ||
+  { echo "Errore durante il load della mappa con /home/maps/$OSM_ID/pgsimple_load_0.6.sql"; exit 1; } 
 cd /home/scripts/
-psql postgresql://$DB_USER:$DB_PASSWORD@postgres:5432/$DB_NAME -f /home/scripts/performance_optimization.sql -v OSM_ID=$OSM_ID
+psql postgresql://$DB_USER:$DB_PASSWORD@postgres:5432/$DB_NAME -f /home/scripts/performance_optimization.sql -v OSM_ID=$OSM_ID ||
+  { echo "Errore durante l'esecuzione di /home/scripts/performance_optimization.sql sul db"; exit 1; } 
